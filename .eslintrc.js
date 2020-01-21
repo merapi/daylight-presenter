07fdd70e81dc1989853adb04aa74a64155f1db9e
merapi <marcin@webskill.pl>
2020-01-21 11:46:44 +0100

    Typescript/Prettier/ESLint configs

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    semi: ["error", "never"],
+    "@typescript-eslint/explicit-function-return-type": [
+      "off",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
